3b0841c8c3388a5a56a85c91e6697f49aae87052
Dijosto <32780744+Dijosto@users.noreply.github.com>
2025-01-29 11:45:45 -0600

    Adding the Windows Form I've attempted to make
    For some reason it fails copying the files and then causes a big mess, requiring reinstall.

---
+++ SandFoxGUI/Form1.Designer.cs
@@ -0,0 +1,45 @@
+﻿namespace SandFoxGUI
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.btnInject = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnInject
+            // 
+            this.btnInject.Location = new System.Drawing.Point(100, 100);
+            this.btnInject.Name = "btnInject";
+            this.btnInject.Size = new System.Drawing.Size(100, 23);
+            this.btnInject.TabIndex = 0;
+            this.btnInject.Text = "Inject";
+            this.btnInject.UseVisualStyleBackColor = true;
+            this.btnInject.Click += new System.EventHandler(this.btnInject_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 253);
+            this.Controls.Add(this.btnInject);
+            this.Name = "Form1";
+            this.Text = "Native Payload Injector";
+            this.ResumeLayout(false);
+        }
+
+        private System.Windows.Forms.Button btnInject;
+    }
+}
+
---
+++ SandFoxGUI/Form1.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Windows.Forms;
+
+namespace SandFoxGUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnInject_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var injector = new NativePayloadInjector("sbox");
+                injector.Inject();
+                MessageBox.Show("Injection successful!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
+
